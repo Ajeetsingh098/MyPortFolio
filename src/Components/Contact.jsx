728a3f58c86cdabf6dd958b51fbc7e2a2cf81c67
Ajeet Singh <ajeet@example.com>
2026-05-04 21:57:39 +0530

    email Template change

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -12,10 +12,12 @@ const Contact = () => {
     e.preventDefault();
     setIsSending(true);
 
+    const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID; 
+    const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+
     
-    const SERVICE_ID = "Singh@098"; 
-    const TEMPLATE_ID = "template_wjuhoya";
-    const PUBLIC_KEY = "V-KYWTymZnCFp64F4";
 
     emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
       .then((result) => {
